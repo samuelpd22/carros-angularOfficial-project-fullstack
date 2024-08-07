a809346ee05af1aaac26841824ce22991b36086e
samuelpd22 <samuel050101@hotmail.com>
2024-08-07 20:24:12 -0300

    init: inicio do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { CarroslistComponent } from './components/carros/carroslist/carroslist.component';
+import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",redirectTo:"login", pathMatch:"full"},
+  {path:"login", component: LoginComponent},
+  {path:"admin",component:PrincipalComponent, children: [ //Rota filha, tudo que estiver aqui, será renderizado apos o /admin
+    {path:"carros",component:CarroslistComponent},
+    {path:"carros/new", component:CarrosdetailsComponent},
+    {path:"carros/edit/:id",component:CarrosdetailsComponent}
+
+  ]}
+];
