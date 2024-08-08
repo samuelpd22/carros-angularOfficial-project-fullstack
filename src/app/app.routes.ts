6c2aa6597db6bc3ca1d45972d1fc511e98f71add
samuelpd22 <samuel050101@hotmail.com>
2024-08-08 19:49:14 -0300

    feat: rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { CarroslistComponent } from './components/carros/carroslist/carroslist.component';
 import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
+import { MarcaslistComponent } from './components/marcas/marcaslist/marcaslist.component';
+import { MarcasdetailsComponent } from './components/marcas/marcasdetails/marcasdetails.component';
 
 export const routes: Routes = [
   {path:"",redirectTo:"login", pathMatch:"full"},
@@ -10,7 +12,10 @@ export const routes: Routes = [
   {path:"admin",component:PrincipalComponent, children: [ //Rota filha, tudo que estiver aqui, será renderizado apos o /admin
     {path:"carros",component:CarroslistComponent},
     {path:"carros/new", component:CarrosdetailsComponent},
-    {path:"carros/edit/:id",component:CarrosdetailsComponent}
+    {path:"carros/edit/:id",component:CarrosdetailsComponent},
+    {path:"marcas",component:MarcaslistComponent},
+    {path:"marcas/new", component:MarcasdetailsComponent},
+    {path:"marcas/edit/:id",component:MarcasdetailsComponent},
 
   ]}
 ];
