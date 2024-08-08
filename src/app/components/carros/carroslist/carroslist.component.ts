522290dd53afcdccb1ef90ad33a356cc1bce8e8c
samuelpd22 <samuel050101@hotmail.com>
2024-08-08 19:49:50 -0300

    feat: carroslist

--- src/app/components/carros/carroslist/carroslist.component.ts
+++ src/app/components/carros/carroslist/carroslist.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { Carro } from '../../../models/carro';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-carroslist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './carroslist.component.html',
   styleUrl: './carroslist.component.scss'
 })
 export class CarroslistComponent {
+
+
+  lista: Carro[]=[];
+
+  constructor(){
+
+    this.lista.push(new Carro(1, 'Fiesta'));
+    this.lista.push(new Carro(2, 'i30'));
+    this.lista.push(new Carro(3, 'Corolla'));
+    this.lista.push(new Carro(4, 'Civic'));
+
+
+    let carroNovo = history.state.carroNovo;
+    let carroEditado = history.state.carroEditado;
+
+    if(carroNovo){
+      carroEditado.id = 555;
+      this.lista.push(carroNovo);
+
+    }
+    if(carroEditado){
+      let indice = this.lista.findIndex(x => {return x.id == carroEditado.id});
+      this.lista[ indice ] = carroEditado;
+    }
+
+  }
+
+  deleteById(Carro: Carro){
+    if(confirm("Tem certeza que deseja deletar este registro?") ){
+    let indice = this.lista.findIndex(x => {return x.id == Carro.id})
+    this.lista.splice(indice, 1);
+    }
+  }
 
 }
