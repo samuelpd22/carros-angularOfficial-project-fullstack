e10a0c8c6e268f66e4631eb798494034a7685c80
samuelpd22 <samuel050101@hotmail.com>
2024-08-08 19:49:35 -0300

    feat: carros details

--- src/app/components/carros/carrosdetails/carrosdetails.component.ts
+++ src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { MdbFormsModule, } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { Carro } from '../../../models/carro';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrosdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule,FormsModule],
   templateUrl: './carrosdetails.component.html',
   styleUrl: './carrosdetails.component.scss'
 })
 export class CarrosdetailsComponent {
 
+    carro:Carro = new Carro(0,'');
+    router = inject(ActivatedRoute);
+    router2= Inject(Router);
+
+    constructor(){
+      let id = this.router.snapshot.params['id'];
+      if(id > 0){
+        this.findById(id)
+      }
+    }
+
+    findById(id:number){
+      //busca back-end
+      let carroRetornado: Carro = new Carro(id, "Fiesta")
+      this.carro = carroRetornado;
+    }
+    save(){
+      if(this.carro.id > 0){
+        alert('Editado com sucesso!');
+        this.router2.navigate(['admin/carros'], { state:{ carroEditado: this.carro} });
+      } else {
+        alert('Salvo com sucesso!');
+        this.router2.navigate(['admin/carros'], { state:{ carroNovo: this.carro} });
+    }
+    }
 }
