0f4e2cd3f20eca025c38c9792cec80b17e122429
samuelpd22 <samuel050101@hotmail.com>
2024-08-13 20:55:02 -0300

    feat: add metodos ts dos components

--- src/app/components/marcas/marcasdetails/marcasdetails.component.ts
+++ src/app/components/marcas/marcasdetails/marcasdetails.component.ts
@@ -1,12 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter,  inject, Input, Output, output } from '@angular/core';
+import { MdbFormsModule, } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { Marca } from '../../../models/marca';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { MarcaService } from '../../../services/marca.service';
+
+
 
 @Component({
   selector: 'app-marcasdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './marcasdetails.component.html',
   styleUrl: './marcasdetails.component.scss'
 })
 export class MarcasdetailsComponent {
 
+  marcaService = inject(MarcaService);//@Autowired
+
+    @Input("marca") marca:Marca = new Marca(0,'');
+    @Output("retorno") retorno = new EventEmitter<any>();
+    router = inject(ActivatedRoute);
+    router2 = inject(Router);
+
+    constructor(){
+      let id = this.router.snapshot.params['id']; //Pega o ID que veio por parametro na ROTA HTTP
+      if(id > 0){
+        this.findById(id)
+      }
+    }
+
+    findById(id:number){
+      //busca back-end
+
+      this.marcaService.findById(id).subscribe({
+        next: retorno => {
+          this.marca = retorno;
+        },
+        error: erro => {
+          Swal.fire({   //EXIBE MENSAGEL
+            title:"Ocorreu um erro",
+            icon:'error',
+            confirmButtonText: 'Ok'
+          });
+        }
+      })
+    }
+
+      //__________________________________________________________________________________________________________________//
+      // Editar
+    save(){
+      if(this.marca.id > 0){
+
+        this.marcaService.update(this.marca, this.marca.id).subscribe({ //UPDATE
+          next: mensagem => {
+            Swal.fire({   //EXIBE MENSAGEM
+              title:mensagem,
+              icon:'success',
+              confirmButtonText: 'Ok'
+            });
+            this.router2.navigate(['admin/marcas'], { state:{ carroEditado: this.marca} });
+            this.retorno.emit(this.marca);
+          },
+          error: erro => {
+            Swal.fire({   //EXIBE MENSAGEM
+              title:"Ocorreu um erro",
+              icon:'error',
+              confirmButtonText: 'Ok'
+            });
+          }
+        })
+
+
+      //__________________________________________________________________________________________________________________//
+      // Salvar
+      } else {
+
+
+        this.marcaService.save(this.marca).subscribe({ //SALVAR
+          next: mensagem => {
+            Swal.fire({   //EXIBE MENSAGEM
+              title:mensagem,
+              icon:'success',
+              confirmButtonText: 'Ok'
+            });
+             //FECHAR MODAL
+            this.router2.navigate(['admin/marcas'], { state:{ carroNovo: this.marca} });
+            this.retorno.emit(this.marca);
+          },
+          error: erro => {
+            Swal.fire({   //EXIBE MENSAGEL
+              title:"Ocorreu um erro",
+              icon:'error',
+              confirmButtonText: 'Ok'
+            });
+          }
+        });
+
+
+        //FECHAR MODAL
+        this.router2.navigate(['admin/marcas'], { state:{ carroNovo: this.marca} });
+      }
+
+
+    }
 }
--- src/app/components/marcas/marcaslist/marcaslist.component.ts
+++ src/app/components/marcas/marcaslist/marcaslist.component.ts
@@ -1,12 +1,126 @@
-import { Component } from '@angular/core';
+
+import { Component, inject, TemplateRef, ViewChild, viewChild } from '@angular/core';
+import { Carro } from '../../../models/carro';
+import { RouterLink } from '@angular/router';
+import { MdbModalModule, MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { MarcasdetailsComponent } from "../marcasdetails/marcasdetails.component";
+import Swal from 'sweetalert2';
+import { MarcaService } from '../../../services/marca.service';
+import { Marca } from '../../../models/marca';
+
 
 @Component({
   selector: 'app-marcaslist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MdbModalModule, MarcasdetailsComponent,MarcasdetailsComponent],
   templateUrl: './marcaslist.component.html',
   styleUrl: './marcaslist.component.scss'
 })
 export class MarcaslistComponent {
+
+  lista: Marca[]=[];
+  marcaEdit: Marca = new Marca(0,"");
+
+  //ELEMENTOS DE MODAL
+  modalService = inject(MdbModalService); // Para conseguir abrir a MODAL
+  @ViewChild("modalMarcaDetalhe") modelMarcaDetalhe!: TemplateRef<any>;
+  modalRef!: MdbModalRef<any>;
+  //ELEMENTOS DE MODAL ^^^
+
+
+
+  marcaService = inject(MarcaService);//@Autowired
+
+
+  constructor(){
+
+   this.listAll();
+
+
+    let marcaNovo = history.state.marcaNovo;
+    let marcaEditada = history.state.marcaEditada;
+
+    if(marcaNovo){
+      marcaNovo.id = 555;
+      this.lista.push(marcaNovo);
+
+    }
+    if(marcaEditada){
+      let indice = this.lista.findIndex(x => {return x.id == marcaEditada.id});
+      this.lista[ indice ] = marcaEditada;
+    }
+
+  }
+
+  listAll(){          //ESTRUTURA PARA METODOD GET RETORNAR
+    this.marcaService.listAll().subscribe({
+      next:lista => { //QUANDO DER CERTO
+        this.lista = lista;
+
+      },
+      error: err => { //QUANDO OCORRER ERRO
+        Swal.fire({   //EXIBE MENSAGEL
+          title:"Ocorreu um erro",
+          icon:'error',
+          confirmButtonText: 'Ok'
+        });
+      },
+    });
+  }
+
+
+
+  deleteById(marca: Marca){
+    Swal.fire({
+      title:'Tem certeza que deseja deletar este registro?',
+      icon:'warning',
+      showConfirmButton:true,
+      showDenyButton:true,
+      confirmButtonText:' Sim',
+      cancelButtonText:'Não',
+
+    }).then((result) =>{
+      if(result.isConfirmed){ //Clicou em OK! para deletar, vai fazer:
+
+
+        this.marcaService.delete(marca.id).subscribe({
+          next:message => { //QUANDO DER CERTO
+            Swal.fire({   //EXIBE MENSAGEL
+              title:message, //PEGA MENSAGEL DO MEU BACKEND , No endpoint Delete
+              icon:'success',
+              confirmButtonText: 'Ok'
+            });
+            this.listAll();
+          },
+          error: err => { //QUANDO OCORRER ERRO
+            Swal.fire({   //EXIBE MENSAGEL
+              title:"Ocorreu um erro",
+              icon:'error',
+              confirmButtonText: 'Ok'
+            });
+          },
+        });
+
+
+      }
+    })
+
+  }
+
+  new(){
+    this.marcaEdit = new Marca(0,"");
+    this.modalRef = this.modalService.open(this.modelMarcaDetalhe);
+  }
+  edit(marca:Marca){
+    this.marcaEdit = Object.assign({}, marca);//CLOANDO PRA EVITAR REFERENCIA DE OBJETO
+    this.modalRef = this.modalService.open(this.modelMarcaDetalhe);
+
+  }
+
+
+  retornoDetalhe(marca:Marca){
+    this.listAll();
+    this.modalRef.close();
+  }
 
 }
