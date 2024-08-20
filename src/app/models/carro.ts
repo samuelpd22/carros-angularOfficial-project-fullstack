bbf66ce81ff69b5c5001875206f0e564ee8a8a7b
samuelpd22 <samuel050101@hotmail.com>
2024-08-20 18:04:10 -0300

    feat : add marca a model de carros

--- src/app/models/carro.ts
+++ src/app/models/carro.ts
@@ -1,10 +1,17 @@
+import { Marca } from "./marca";
+
 export class Carro {
   id!:number;
   nome!:string;
+  marca!:Marca;
 
 
-  constructor(id:number,nome:string){
+  constructor(id:number,nome:string, marca:Marca | null){
     this.id = id;
     this.nome = nome;
+
+    if(marca) this.marca = marca;
+
   }
+
 }
