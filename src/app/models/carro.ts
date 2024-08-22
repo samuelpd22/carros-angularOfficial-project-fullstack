183467b475bc3de8f6fbf8c790ab6191e6adca54
samuelpd22 <samuel050101@hotmail.com>
2024-08-22 18:38:01 -0300

    feat : add novo atributo lista de acessorios

--- src/app/models/carro.ts
+++ src/app/models/carro.ts
@@ -1,9 +1,11 @@
+import { Acessorios } from "./acessorios";
 import { Marca } from "./marca";
 
 export class Carro {
   id!:number;
   nome!:string;
   marca!:Marca;
+  acessorios:Acessorios[] = [];
 
 
   constructor(id:number,nome:string, marca:Marca | null){
