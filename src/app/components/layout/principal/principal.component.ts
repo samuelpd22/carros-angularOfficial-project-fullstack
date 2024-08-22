14a104086204e0b96fe0628057024be478e49838
samuelpd22 <samuel050101@hotmail.com>
2024-08-22 18:38:30 -0300

    feat: add atributos fixos ao layout

--- src/app/components/layout/principal/principal.component.ts
+++ src/app/components/layout/principal/principal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [MenuComponent,RouterOutlet],//Importar para usar no HTML outros componentes por TAG
+  imports: [MenuComponent,RouterOutlet ,SidebarComponent],//Importar para usar no HTML outros componentes por TAG
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
